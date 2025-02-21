394ccec469736709a30c586a2c54bac00e48f565
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-02-21 11:36:56 +0530

    image - core components

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,16 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "red" }}>
+    <View style={{ flex: 1, backgroundColor: "red", padding: 30 }}>
+      <Image source={logoImg} style={{ flexheight: 400, width: 400 }}></Image>
       <View style={{ height: 100, width: 100, backgroundColor: "blue" }}></View>
-      <View style={{ height: 100, width: 100, backgroundColor: "green" }}>
-        <Text>
-          <Text style={{color: "white"}}>Hello</Text>
-          {" Jay"}
-        </Text>
-      </View>
     </View>
   );
 }
