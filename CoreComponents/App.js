ed661ad2fd887e62b6ef07eb33046e9740205358
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-02-21 11:32:31 +0530

    View - core components

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>inside CoreComponents to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "red" }}>
+      <View style={{ height: 100, width: 100, backgroundColor: "blue" }}></View>
+      <View style={{ height: 100, width: 100, backgroundColor: "green" }}>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
