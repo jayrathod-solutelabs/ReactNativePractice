0e75acdb3d369ff676519761126d1822ef482400
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-02-21 16:48:51 +0530

    custom components

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,23 +1,13 @@
 
 import { View, Button, Alert, ActivityIndicator, Text } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png");
+import Greet from "./components/Greet";
 
 
 export default function App() {
 
   return (
     <View style={{ flex: 1, backgroundColor: "red", padding: 60 }}>
-      <ActivityIndicator size="large" color="green" />
-      <Button title="Click Here" onPress={() => Alert.alert("Invalid Data", "Please enter valid data", [
-        {
-          text: "OK",
-          onPress: () => console.log("OK Pressed")
-        },
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed")
-        }
-      ])} /> 
+      <Greet name="John" />
     </View>
 
   );
