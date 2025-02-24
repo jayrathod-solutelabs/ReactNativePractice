a37289dc8c55b4878f8e47c205689559568dc146
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-02-24 16:39:28 +0530

    initial flex box

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,26 +1,24 @@
-
-import { View, Text, StyleSheet } from "react-native";
-
+import { StyleSheet, View } from "react-native";
+import Box from "./components/box";
 
 export default function App() {
-
   return (
     <View style={styles.container}>
-      <Text style={styles.Text}>Hello</Text>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
   },
-  Text: {
-    color: 'red',
-    fontSize: 30
-  }
 });
