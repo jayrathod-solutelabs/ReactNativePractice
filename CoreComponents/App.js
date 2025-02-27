2ed358abd28f50412c342f4d02acdc878022cd24
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-02-27 15:05:28 +0530

    flex property

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#b65d1f", alignself: "auto" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
@@ -17,6 +17,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    height: 400,
+    flexWrap: "wrap",
+    alignContent : "space-between",
+
+    // flexDirection: "row",
+    // height:300,
+    // flexWrap: "wrap-reverse",
+    
+    // flexDirection: "column",
+    // alignItems: "flex-start",
+    // justifyContent: "flex-start",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
