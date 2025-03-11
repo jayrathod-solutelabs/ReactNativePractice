0a499c2e8319a7f57d87596707d3c3178b206a91
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-03-11 18:30:19 +0530

    add bottom tabs

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -1,18 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="About" component={AboutScreen} />
+
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- RNNavigation/screens/AboutScreen.js
+++ RNNavigation/screens/AboutScreen.js
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export default function AboutScreen({route}) {
-  const {name} = route.params
+export default function AboutScreen() {
   return (
     <View>
-      <Text>About Screen Name is : {name}</Text>
+      <Text>About Screen Name is</Text>
     </View>
   );
 } 
