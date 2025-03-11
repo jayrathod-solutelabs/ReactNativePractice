73d8b1978f3bff4cf171ccfa533e11c6efd2ebf8
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-03-11 16:59:37 +0530

    Navigation home page to about us page

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -9,7 +9,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='About'>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="About" component={AboutScreen} />
       </Stack.Navigator>
--- RNNavigation/screens/HomeScreen.js
+++ RNNavigation/screens/HomeScreen.js
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native';
+import { View, Text ,Button } from 'react-native';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home Screen</Text>
+      <Button title='Go to About' onPress={() => navigation.navigate('About')}>
+      
+      </Button>
     </View>
   );
 } 
