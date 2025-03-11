add15edea2c172ae4aaee1b3be7ca026e55222d9
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-03-11 11:02:40 +0530

    pull to refresh

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -5,9 +5,10 @@ export default function App() {
 
   const [postList, setPostList] = useState([]);
   const [isLoading, setisLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
 const fetchData = async(limit = 10) => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=20');
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
   const data = await response.json();
   setPostList(data);
   setisLoading(false);
@@ -17,6 +18,12 @@ const fetchData = async(limit = 10) => {
 useEffect(() => {
   fetchData();
 }, []);
+
+const handleRefresh = () => {
+  setRefreshing(true);
+  fetchData(20);
+  setRefreshing(false);
+};
 
 if (isLoading) { 
   return (
@@ -38,6 +45,8 @@ if (isLoading) {
               <Text style={styles.bodyText}>{item.body}</Text>
             </View>
           )}
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
         />
       </View>
     </SafeAreaView>
