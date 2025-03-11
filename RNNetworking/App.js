5a3a473bd5b96a565646cb1282c7f27220e96482
jayrathod-solutelabs <jay.r1@solutelabs.com>
2025-03-11 10:41:07 +0530

    add loading indicator

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -1,20 +1,31 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, FlatList} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, FlatList, ActivityIndicator} from 'react-native';
 import { useEffect, useState } from 'react';
 
 export default function App() {
 
   const [postList, setPostList] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
 
 const fetchData = async(limit = 10) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=20');
   const data = await response.json();
   setPostList(data);
+  setisLoading(false);
 
 }
 
 useEffect(() => {
   fetchData();
 }, []);
+
+if (isLoading) { 
+  return (
+    <SafeAreaView style={styles.LoadingContainer}>
+      <ActivityIndicator size="large" color="#0000ff" />
+      <Text>Loading...</Text>
+    </SafeAreaView>
+  );
+}
 
   return (
     <SafeAreaView style={styles.container}>
@@ -56,5 +67,12 @@ const styles = StyleSheet.create({
   },
   bodyText: {
     fontSize: 16,
+  },
+  LoadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight
   },
 });
